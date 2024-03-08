b005648052e6a626d1a6c6742ac6641360fad8ae
Liston Fermi <listonfermi@gmail.com>
2024-03-08 20:34:36 +0530

    2024-03-08

---
+++ 1791. Find Center of Star Graph 2024-03-08.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var findCenter = function(edges) {
+    const edgeFlat = edges.flat()
+    const f={}
+ 
+     for(let v of edgeFlat){
+         if(f[v]) f[v]++
+         else f[v]=1
+     }
+ 
+     const mostOccured=  Object.entries(f).reduce( (acc,curr)=>{
+         return acc= acc[1]>curr[1]?acc:curr
+     },Object.entries(f)[0]  )
+ 
+     return mostOccured[0]
+ 
+ };
