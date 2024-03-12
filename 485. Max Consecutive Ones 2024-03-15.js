14ba99e23926ccd301e200bd96c6099d5fbe91e5
Liston Fermi <listonfermi@gmail.com>
2024-03-12 09:27:18 +0530

    2024-03-12

---
+++ 485. Max Consecutive Ones 2024-03-15.js
@@ -0,0 +1,16 @@
+var findMaxConsecutiveOnes = function(nums) {
+    let count=0, L=0, ans=[]
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===1){
+            count++
+            if(i===nums.length-1) L= count>L?count:L
+        }else if( (nums[i-1]===1 && nums[i]!=1)){
+            L= count>L?count:L
+            count=0
+        }
+    }
+    return L
+};
+
+const nums = [1,1,0,1,1,1]
+console.log(findMaxConsecutiveOnes(nums));
