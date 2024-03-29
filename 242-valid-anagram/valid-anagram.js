cc91ccfeef20708a74e565aaae71ccd4ad0de34d
ListonFermi <145888971+ListonFermi@users.noreply.github.com>
2024-03-29 16:42:15 +0530

    Time: 80 ms (33.51%) | Memory: 54 MB (31.51%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    return s.split('').sort().join('') === t.split('').sort().join('')
+}
+
+
+
+// var isAnagram = function(s, t) {
+//     return s.split('').sort().join('')=== t.split('').sort().join('')
+// };
