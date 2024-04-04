84241bd98d8ac832962ad0c7007d9569e7f07df0
ListonFermi <145888971+ListonFermi@users.noreply.github.com>
2024-04-04 09:57:38 +0530

    Time: 50 ms (78.57%) | Memory: 50.2 MB (30.95%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.js
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.js
@@ -4,18 +4,18 @@
  * @return {boolean}
  */
 var areAlmostEqual = function (s1, s2) {
-    if(s1===s2) return true
-    const sameChars = s1.split('').sort().join('') ===s2.split('').sort().join('');
-    console.log('2')
+    // if(s1===s2) return true
 
+    const sameChars = s1.split('').sort().join('') ===s2.split('').sort().join('');
     if(!sameChars) return false
+
     let count=0
     for(let i=0;i<s1.length;i++){
         if(s1[i]!=s2[i]){
             count++
         }
     }
-    console.log('3')
-    if(count===2) return true
+
+    if(count===2 || count===0) return true
     else return false
 }
