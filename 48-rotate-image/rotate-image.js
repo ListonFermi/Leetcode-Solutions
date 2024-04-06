a13b6aba66450eb8a0c4c873fc6d229eb073ac59
ListonFermi <145888971+ListonFermi@users.noreply.github.com>
2024-04-07 00:13:22 +0530

    Time: 61 ms (15.17%) | Memory: 50.8 MB (17.77%) - LeetSync

--- 48-rotate-image/rotate-image.js
+++ 48-rotate-image/rotate-image.js
@@ -3,15 +3,15 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function (matrix) {
-    let swapped = []
+    let swapped = {}
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
 
-            if (!swapped.includes(`${i, j}`)) {
+            if (!swapped[`${i, j}`]) {
 
                 [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
 
-                swapped.push(`${j, i}`)
+                swapped[`${j, i}`] = true
 
             }
 
