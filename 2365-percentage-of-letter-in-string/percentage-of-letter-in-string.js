636f80799ad82b4171b5ffefb1ffb4b6fd2edc66
ListonFermi <145888971+ListonFermi@users.noreply.github.com>
2024-04-11 09:17:21 +0530

    Time: 46 ms (86.18%) | Memory: 47.9 MB (98.62%) - LeetSync

--- 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.js
+++ 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.js
@@ -4,10 +4,11 @@
  * @return {number}
  */
 var percentageLetter = function(s, letter) {
-    s= s.split(''), f={}
+    s= s.split('')
+    const f={}
+    f[letter] =0
     for(let val of s ){
-        if(f[val]) f[val]++
-        else f[val]=1
+        if(val===letter) f[letter]++
     }
     if(!f[letter]) return 0
     return (Math.floor(f[letter]*100/s.length))
