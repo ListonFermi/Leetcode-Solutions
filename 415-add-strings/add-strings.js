fc4f656d6172c9b4c86ea2e5ab8751269ab956b1
ListonFermi <145888971+ListonFermi@users.noreply.github.com>
2024-04-30 09:23:18 +0530

    Time: 49 ms (91.19%) | Memory: 48.1 MB (99.67%) - LeetSync

---
+++ 415-add-strings/add-strings.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    // let n1= Math.max(num1,num2), n2= Math.min(num1,num2)
+
+    // for(let i)
+
+    return BigInt(num1)+BigInt(num2)+''
+    
+};
