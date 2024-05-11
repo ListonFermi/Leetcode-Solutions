d74de1af5bda9867b03ca34711027f82874cf8a8
ListonFermi <145888971+ListonFermi@users.noreply.github.com>
2024-05-11 22:59:24 +0530

    Time: 62 ms (85.92%) | Memory: 53.1 MB (56.18%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -3,12 +3,33 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    return s.split('').sort().join('') === t.split('').sort().join('')
-}
-
-
-
 // var isAnagram = function(s, t) {
-//     return s.split('').sort().join('')=== t.split('').sort().join('')
-// };
+//     return s.split('').sort().join('') === t.split('').sort().join('')
+// }
+
+var isAnagram = function (s, t) {
+
+    if (s.length !== t.length) return false
+
+    let fS = {}, fT = {}
+    for (let str of s) {
+        if (!fS[str]) fS[str] = 1
+        else fS[str]++
+    }
+    for (let str of t) {
+        if (!fT[str]) fT[str] = 1
+        else fT[str]++
+    }
+    const fSArr = Object.entries(fS)
+    const fTArr = Object.entries(fT)
+
+    if (fSArr.length !== fTArr.length) return false
+
+    for (let i = 0; i < fSArr.length; i++) {
+
+        let str = fSArr[i][0]
+
+        if (fS[str] != fT[str]) return false
+    }
+    return true
+}
