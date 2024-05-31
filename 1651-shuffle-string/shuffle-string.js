ca15082fc2c058d99a682acb8afaf0f686d6bff4
ListonFermi <145888971+ListonFermi@users.noreply.github.com>
2024-05-31 09:22:16 +0530

    Time: 63 ms (34.72%) | Memory: 51.4 MB (7.61%) - LeetSync

--- 1651-shuffle-string/shuffle-string.js
+++ 1651-shuffle-string/shuffle-string.js
@@ -4,6 +4,12 @@
  * @return {string}
  */
 var restoreString = function(s, indices) {
-    const arr = s.split('').map((v,i)=>[v,indices[i]])
-    return arr.sort((a,b)=>a[1]-b[1]).map((v)=>v[0]).join('')
+    // const arr = s.split('').map((v,i)=>[v,indices[i]])
+    // return arr.sort((a,b)=>a[1]-b[1]).map((v)=>v[0]).join('')
+
+    const ans=new Array(s.length)
+    for(let i in s){
+        ans[indices[i]] = s[i]
+    }
+    return ans.join('')
 };
