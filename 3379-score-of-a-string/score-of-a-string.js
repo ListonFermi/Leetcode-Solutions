a3327c48b9aa72e322a381727961f1d9e034418a
ListonFermi <145888971+ListonFermi@users.noreply.github.com>
2024-06-01 09:24:08 +0530

    Time: 75 ms (8.67%) | Memory: 49.7 MB (75.18%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.js
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -2,10 +2,18 @@
  * @param {string} s
  * @return {number}
  */
-var scoreOfString = function(s) {
-    let sum=0
+// var scoreOfString = function(s) {
+//     let sum=0
+//     for(let i=0;i<s.length-1;i++){
+//         sum+= Math.abs(s.charCodeAt(i)-s.charCodeAt(i+1))
+//     }
+//     return sum
+// };
+
+var scoreOfString = function(s){
+    let ans=0
     for(let i=0;i<s.length-1;i++){
-        sum+= Math.abs(s.charCodeAt(i)-s.charCodeAt(i+1))
+        ans+= Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1))
     }
-    return sum
-};
+    return ans
+}
