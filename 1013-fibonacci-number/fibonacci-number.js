5baba8be90ffedc19719261fb4ab84eaf84567ee
Liston Fermi <145888971+ListonFermi@users.noreply.github.com>
2024-07-20 11:12:18 +0530

    Time: 59 ms (53.69%) | Memory: 49.1 MB (8.76%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// var fib = function(n) {
+//     if(n<2) return n
+//     return fib(n - 1) + fib(n - 2)
+// };
+
+var fib = function(n) {
+    if(n<=1) return n
+    return fib(n-1) + fib(n-2)
+}
