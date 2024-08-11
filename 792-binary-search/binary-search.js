3acc5080eff91321eb1e7a47f6c42bea36368372
Liston Fermi <145888971+ListonFermi@users.noreply.github.com>
2024-08-12 02:34:55 +0530

    Time: 55 ms (65.11%) | Memory: 52.2 MB (43.43%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -3,19 +3,30 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (nums, target) {
+// var search = function (nums, target) {
 
-    let l = 0
-    let r = nums.length - 1
+//     let l = 0
+//     let r = nums.length - 1
 
-    while (l <= r) {
-        const m = Math.floor((l + r) / 2)
+//     while (l <= r) {
+//         const m = Math.floor((l + r) / 2)
 
-        if (target === nums[m]) return m
-        else if (target < nums[m]) r = m - 1
-        else l = m + 1
+//         if (target === nums[m]) return m
+//         else if (target < nums[m]) r = m - 1
+//         else l = m + 1
 
-    }
-    return -1
+//     }
+//     return -1
 
-};
+// };
+
+var search = function (nums, t, l = 0, r = nums.length - 1) {
+    const m = Math.floor((l + r) / 2)
+
+    if (t === nums[m]) return m
+
+    if(l>r) return -1
+
+    if (t < nums[m]) return search(nums, t, l, m - 1)
+    else return search(nums, t, m + 1, r)
+}
