403734e3e4d0b33cb16667d6238bc124e6fd77da
Liston Fermi <145888971+ListonFermi@users.noreply.github.com>
2024-08-17 09:45:57 +0530

    Time: 105 ms (81.89%) | Memory: 62 MB (81.59%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -2,42 +2,13 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function (strs) {
-    const res = []
-    for (let i = 0; i < strs.length; i++) {
-        if (strs[i] === null) continue
-        res.push([strs[i]])
-        for (let j = i + 1; j < strs.length; j++) {
-            if (strs[j] === null) continue           
-            if (isAnagram(strs[i], strs[j])) {
-                res[res.length - 1].push(strs[j])
-                strs[j] = null
-            }
-        }
+var groupAnagrams = function(strs) {
+    const obj ={}
+    let sortedStr
+    for(let str of strs){
+        sortedStr= str.split('').sort().join('')
+        if(obj[sortedStr]) obj[sortedStr].push(str)
+        else obj[sortedStr] = [str]
     }
-    return res
+    return Object.values(obj)
 };
-
-var isAnagram = function (s, t) {
-
-    if(s.length != t.length) return false
-
-    const sF = new Array(26).fill(0)
-    const tF = new Array(26).fill(0)
-    let index
-
-    for(let i=0;i<s.length;i++){
-        index = s[i].charCodeAt(0) - 'a'.charCodeAt(0)
-        sF[index]++
-    }
-
-    for(let i=0;i<t.length;i++){
-        index = t[i].charCodeAt(0) - 'a'.charCodeAt(0)
-        tF[index]++
-    }
-
-    for(let i=0;i<26;i++){
-        if(sF[i]!=tF[i]) return false
-    }
-    return true
-}
