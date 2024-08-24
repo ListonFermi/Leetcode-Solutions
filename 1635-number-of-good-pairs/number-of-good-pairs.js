8dc615d3a7c59a5c0efc8352f9161648766fa894
Liston Fermi <145888971+ListonFermi@users.noreply.github.com>
2024-08-24 09:24:41 +0530

    Time: 63 ms (7.18%) | Memory: 48.6 MB (83.99%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.js
+++ 1635-number-of-good-pairs/number-of-good-pairs.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var numIdenticalPairs = function(nums) {
-    let count=0
+    let count = 0
     for(let i=0;i<nums.length;i++){
         for(let j=i+1;j<nums.length;j++){
-            if(nums[i]===nums[j]) count++
+            if(nums[i]=== nums[j]) count++
         }
     }
     return count
