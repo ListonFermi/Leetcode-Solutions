9f5ac1b830be7dfc621f0d040a9dbabb7e49f6fd
Liston Fermi <145888971+ListonFermi@users.noreply.github.com>
2024-09-02 10:11:31 +0530

    Time: 71 ms (7.52%) | Memory: 51.2 MB (47.37%) - LeetSync

--- 944-smallest-range-i/smallest-range-i.js
+++ 944-smallest-range-i/smallest-range-i.js
@@ -3,8 +3,8 @@
  * @param {number} k
  * @return {number}
  */
-var smallestRangeI = function(nums, k) {
-    nums.sort((a,b)=>b-a)
-    const range = (nums[0] - k) - (nums[nums.length-1] + k)
-    return  range < 0 ? 0 : range
+var smallestRangeI = function (nums, k) {
+    nums.sort((a, b) => b - a)
+    const range = (nums[0] - k) - (nums[nums.length - 1] + k)
+    return range > 0 ? range : 0
 };
