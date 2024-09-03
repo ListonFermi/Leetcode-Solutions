15ea4870a3064c1b2250f9146e065fd9fee4f94f
Liston Fermi <145888971+ListonFermi@users.noreply.github.com>
2024-09-03 09:35:45 +0530

    Time: 140 ms (14.62%) | Memory: 84.3 MB (5.97%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+    const f = {}
+    let curr, lastInd
+    for (let i = 0; i < nums.length; i++) {
+        curr = nums[i]
+        if (f[curr]) {
+            lastInd = f[curr][f[curr].length - 1]
+            if (i - lastInd <= k) {
+                console.log({ f, lastInd, i, abs: Math.abs(lastInd, i), cond: Math.abs(lastInd, i) <= k })
+                return true
+            } else {
+                f[curr].push(i)
+            }
+        } else {
+            f[nums[i]] = [i]
+        }
+    }
+    return false
+};
