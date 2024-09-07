68c0ee88d7c0b31b80105aa1b8029257489c8f6e
Liston Fermi <145888971+ListonFermi@users.noreply.github.com>
2024-09-07 23:59:55 +0530

    Time: 112 ms (22.86%) | Memory: 49.7 MB (68.82%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,8 +3,8 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (numbers, target) {
-    for (let i = 0; i < numbers.length; i++) {
+var twoSum = function(numbers, target) {
+     for (let i = 0; i < numbers.length; i++) {
         for (let j = i + 1; j < numbers.length; j++) {
             if (numbers[i] + numbers[j] === target)
                 return [i, j]
@@ -12,49 +12,3 @@ var twoSum = function (numbers, target) {
     }
     return []
 };
-
-
-/*
-2024-Feb-18
-
-var twoSum = function(numbers, target) {
-    let i=0, j= numbers.length-1
-    while(i<j){
-        if((numbers[i]+numbers[j])===target){
-            return [i,j]
-        }
-
-        if(numbers[j]>target){
-            j--
-            continue
-        }
-
-        i--
-        j--
-    }
-};
-
-
-*/
-
-
-
-
-
-/*
-2023-Sep-29
-Java
-
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++) {
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target) {return new int[] {i,j}; }
-            }
-        }
-    return null;
-    }
-}
-
-
- */
