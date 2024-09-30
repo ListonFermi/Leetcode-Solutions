7f0cd191afe15757173d5352d66c2316ce851334
Liston Fermi <145888971+ListonFermi@users.noreply.github.com>
2024-09-30 09:21:49 +0530

    Time: 67 ms (28.75%) | Memory: 54.6 MB (7.20%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -2,31 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function (nums) {
-    // const f = {}, L = { num: nums[0], f: 1 }
-    // for (let i = 0; i < nums.length; i++) {
-    //     if (f[nums[i]]) {
-    //         f[nums[i]]++
-    //         if (f[nums] > L.f){
-    //             L.num = nums[i]
-    //             L.f = f[nums[i]]
-    //         }   
-    //     }else{
-    //         f[nums[i]] =1
-    //     }
-    // }
-    // return L.num
-
-    const f= {}
+var majorityElement = function(nums) {
+    let freq ={}, L = -Infinity , majElement
     for(let num of nums){
-        if(f[num]) f[num]++
-        else f[num]=1
+        if(freq[num]){
+            freq[num]++
+        }else{
+            freq[num] = 1
+        }
+        if(freq[num]>L){
+            L = freq[num]
+            majElement = num
+        }
     }
-
-    const fArr = Object.entries(f)
-    const ans = fArr.reduce((acc,curr)=>{
-        if(curr[1]> acc[1]) return curr
-        else return acc
-    })
-    return ans[0]   
+    return majElement
 };
