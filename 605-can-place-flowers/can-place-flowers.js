0137b4efc42315ae313262ba8fc43df9afced400
Liston Fermi <145888971+ListonFermi@users.noreply.github.com>
2024-10-13 23:49:27 +0530

    Time: 51 ms (88.40%) | Memory: 53.1 MB (8.77%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+    let possiblePosition = 0
+    let prev, next
+    const potentialPos = {}
+
+    for (let i = 0; i < flowerbed.length; i++) {
+        prev = flowerbed[i - 1]
+        next = flowerbed[i + 1]
+
+        if (i === 0) prev = 0
+
+        if (i === flowerbed.length - 1) next = 0
+
+        if (prev === 0 && next === 0 && flowerbed[i] === 0 && !potentialPos[i-1]) {
+            possiblePosition++
+            potentialPos[i] = true
+        }
+    }
+    return n <= possiblePosition
+};
