cab5d7af256fdc993f282464d41f9736aa726633
Liston Fermi <145888971+ListonFermi@users.noreply.github.com>
2024-10-19 10:24:58 +0530

    Time: 4 ms (100.00%) | Memory: 58.3 MB (29.54%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -3,30 +3,12 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-    let sum = 0, L = -Infinity
+    let L = -Infinity, sum = 0
     for (let num of nums) {
         sum += num
-        L = sum > L ? sum : L
+        if (sum > L) L = sum
+
         if (sum < 0) sum = 0
     }
     return L
 };
-
-
-/*
-Brute force: O(n^2)  test case 203/210
-var maxSubArray = function (nums) {
-    let sum, L = -Infinity
-    sum = nums[0]
-    L = sum > L ? sum : L
-    for (let i = 0; i < nums.length; i++) {
-        sum = nums[i]
-        L = sum > L ? sum : L
-        for (let j = i + 1; j < nums.length; j++) {
-            sum += nums[j]
-            L = sum > L ? sum : L
-        }
-    }
-    return L
-};
-*/
